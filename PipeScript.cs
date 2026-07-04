c63418f6f417d7c8d1b6667ad21eb5a89c077593
papoltheeb49 <papolthee.b@kkumail.com>
2026-07-04 19:13:22 +0700

    Add files via upload

---
+++ PipeScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PipeScript : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float deadZone = -15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        if (transform.position.x < deadZone)
+            Destroy(gameObject);
+    }
+}
